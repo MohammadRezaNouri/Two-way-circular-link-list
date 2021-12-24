18363bed19469aadca547e27f01278352364b98e
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2021-12-25 03:15:52 +0330

    Definition of class CDLL in CDLL.h

--- include/CDLL.h
+++ include/CDLL.h
@@ -1,6 +1,18 @@
 #ifndef CDLL_H
 #define CDLL_H
 
+#include "node.h"
 
+class CDLL
+{
+private:
+    node * head, *tail;
+public:
+    CDLL();
+    void insert(int);
+    void print();
+    void deleteByKey(int);
+    void deleteAtPosition(int);
+};
 
 #endif
