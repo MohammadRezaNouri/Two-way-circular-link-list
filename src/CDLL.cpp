5765c0c0998cf6412cc0dabbf93264bfe871c897
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2021-12-25 03:24:37 +0330

    Define functions in CDLL.cpp

--- src/CDLL.cpp
+++ src/CDLL.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 
 #include "CDLL.h"
+
+CDLL::CDLL()
+{
+}
+
+void CDLL::insert(int d)
+{
+}
+
+void CDLL::print()
+{
+}
+
+void CDLL::deleteByKey(int d)
+{
+}
+
+void CDLL::deleteAtPosition(int d)
+{
+}
