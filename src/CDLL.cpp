31075a7151c06139e0f33812c23b76d59c9d1424
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2021-12-25 03:34:06 +0330

    Implementing the insert function in CDLL.cpp

--- src/CDLL.cpp
+++ src/CDLL.cpp
@@ -9,6 +9,24 @@ CDLL::CDLL()
 
 void CDLL::insert(int d)
 {
+    node *temp = new node(d);
+    if (head == nullptr && head == tail && tail == nullptr) // link list is empty
+    {
+        head = tail = temp;
+        head->next = tail->next = temp;
+        head->prev = tail->prev = temp;
+    }
+    else
+    {
+        node *curr = head;
+        while (curr->text != head) // find end element in link list
+            curr = curr->next;
+        curr->next = temp;
+        temp->prev = curr;
+        temp.next = head;
+        head->prev = temp;
+        tail = temp;
+    }
 }
 
 void CDLL::print()
