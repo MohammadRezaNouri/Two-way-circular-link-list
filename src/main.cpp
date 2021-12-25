9f30139e543fde5b0a642c0fa7c2d18ce1d5c02e
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2021-12-25 04:12:01 +0330

    Write two test case in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,62 @@
 
 int main()
 {
+    std::cout << "Hi\ntest case 1 :\n";
+    CDLL list1;
+
+    list1.insert(5);
+
+    list1.deleteByKey(5);
+    list1.print();
+
+    list1.insert(6);
+    list1.print();
+
+    list1.insert(7);
+    list1.insert(7);
+    list1.insert(7);
+    list1.insert(7);
+    list1.insert(7);
+
+    list1.deleteAtPosition(12);
+
+    list1.insert(7);
+    list1.insert(8);
+    list1.print();
+
+    list1.deleteByKey(8);
+    list1.print();
+
+    CDLL list2;
+    std::cout << "\ntest case 2 :\n";
+
+    list2.insert(1);
+    list2.insert(2);
+    list2.insert(3);
+    list2.insert(4);
+    list2.insert(5);
+    list2.insert(6);
+    list2.insert(7);
+    list2.insert(8);
+    list2.print();
+
+    list2.deleteByKey(1);
+    list2.print();
+
+    list2.deleteByKey(4);
+    list2.print();
+
+    list2.deleteByKey(10);
+    list2.print();
+
+    list2.deleteAtPosition(0);
+    list2.print();
+
+    list2.deleteAtPosition(2);
+    list2.print();
+
+    list2.deleteAtPosition(10);
+    list2.print();
 
     return 0;
 }
